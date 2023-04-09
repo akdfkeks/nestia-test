f6daa401d2a765b72b9a722fcbb6ddf0fb90c296
akdfkeks <akdfkeks@gmail.com>
2023-04-09 17:19:15 +0900

    Setting up the swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerConfig } from './configs/SwaggerConfig';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Setup the swagger - [Get] : /docs
+  SwaggerConfig(app);
+
   await app.listen(3000);
 }
 bootstrap();
